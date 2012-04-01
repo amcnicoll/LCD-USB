b8cee631b87eb5fc082550ff040896ee4673bcc9
Anthony McNicoll <anthony.mcnicoll@gmail.com>
2012-03-31 23:33:19 -0400

    Added control GUI. Nearly last revision of code before complete parametrization.

--- control utility/gui.ahk
+++ control utility/gui.ahk
@@ -10,9 +10,9 @@ Gui, Add, Button, x222 y90 w70 h20 , Line 1A
 Gui, Add, Button, x222 y130 w70 h20 , Line 2A
 Gui, Add, Button, x512 y90 w70 h20 , Line 1B
 Gui, Add, Button, x512 y130 w70 h20 , Line 2B
-Gui, Add, Slider, x322 y180 w180 h30 , 25
-Gui, Add, Slider, x322 y220 w180 h30 , 25
-Gui, Add, Slider, x322 y260 w180 h30 , 25
+Gui, Add, Slider, x322 y180 w180 h30 Range0-255 gFan1Slide vFan1, 20
+Gui, Add, Slider, x322 y220 w180 h30 Range0-255 gFan2Slide vFan2, 20
+Gui, Add, Slider, x322 y260 w180 h30 Range0-255 gLightSlide vLight, 20
 Gui, Add, Button, x32 y180 w260 h30 , Backlight On
 Gui, Add, Button, x32 y220 w260 h30 , Backlight Off
 Gui, Add, Button, x32 y260 w260 h30 , Clear LCD's
@@ -58,6 +58,21 @@ Return
 
 ButtonClearLCD's:
 Run, usblcd.exe 2,,Hide
+Return
+
+Fan1Slide:
+Gui, Submit, NoHide
+Run, usblcd.exe 3 %Fan1%,,Hide
+Return
+
+Fan2Slide:
+Gui, Submit, NoHide
+Run, usblcd.exe 4 %Fan2%,,Hide
+Return
+
+LightSlide:
+Gui, Submit, NoHide
+Run, usblcd.exe 5 %Light%,,Hide
 Return
 
 GuiClose:
--- main.c
+++ main.c
@@ -14,25 +14,36 @@
 #define set_input(portdir,pin) portdir &= ~(1<<pin)
 #define set_output(portdir,pin) portdir |= (1<<pin)
 
-// Global variables for storing lines
-uint8_t lna0[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};				// Screen A, Line 0
-uint8_t lna1[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};				// Screen A, Line 1
-uint8_t lnb0[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};				// Screen B, Line 0
-uint8_t lnb1[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};				// Screen B, Line 1
-
 // Used for ongoing transmissions
 uint8_t buff[16];				// Build transmitted strings here
 uint8_t addr;
 
-// Debug
-uint8_t lcdinit[16] = {76, 67, 68, 0, 82, 101, 97, 100, 121, 33, 0, 0, 0, 0, 0, 0};				// String: "LCD Ready!"
-uint8_t usbinit[16] = {85, 83, 66, 0, 82, 101, 97, 100, 121, 33, 0, 0, 0, 0, 0, 0};		// Test string: "USB Ready!"
+// Predefined messages:  "LCD Ready!" and "USB Ready!"
+uint8_t lcdinit[16] = {76, 67, 68, 0, 82, 101, 97, 100, 121, 33, 0, 0, 0, 0, 0, 0};
+uint8_t usbinit[16] = {85, 83, 66, 0, 82, 101, 97, 100, 121, 33, 0, 0, 0, 0, 0, 0};
 
 int main(void)
 {
-	// Lights and fan initialization
+	// Lights, backlights, and fan initialization
 	set_output(DDRD,7);
+	set_output(DDRD,6);
+	set_output(DDRD,5);
 	set_high(PORTD,7);
+
+	TCCR0A = 0xA3;			// Timer0, channels A and B to Fast PWM Mode
+	TCCR0B = 0x05;			// Prescaler is 1024 to give MOSFETS plenty of time
+	OCR0B = 0xFF;			// Rev up Fan 1 for 2s to avoid stalling
+	_delay_ms(2000);
+	OCR0B = 0x7F;			// Reduce Fan 1 to 50% power (127)
+	uchar i;
+	for (i=0;i<3;i++){		// Blink lights to signal life
+		OCR0A = 0xFF;
+		_delay_ms(200);
+		OCR0A = 0x00;
+		_delay_ms(200);
+	}
+	OCR0A = 0x14;			// Init lights at low brightness (20)
+
 
 	// LCD initialization
 	_delay_ms(200);
@@ -40,23 +51,21 @@ int main(void)
 	CHA_LCDclr();
 	CHB_LCDinit();
 	CHB_LCDclr();
-	CHA_LCDstringLine(lcdinit,0);
+	CHA_LCDstringLine(lcdinit,0);	// LCD ready to go!
 
 	// USB initialization
-    wdt_enable(WDTO_1S); // enable 1s watchdog timer
+    wdt_enable(WDTO_1S); 		// Enable 1s watchdog timer
     usbInit();
-    usbDeviceDisconnect(); // enforce re-enumeration
-	uchar i;
-    for(i = 0; i<250; i++) { // wait 1000 ms
-        wdt_reset(); // keep the watchdog happy
-        _delay_ms(4);
+    usbDeviceDisconnect(); 		// Enforce re-enumeration
+    for(i = 0; i<200; i++) { 	// Wait 200 ms
+        wdt_reset();			// Keep the watchdog happy
+        _delay_ms(1);
     }
     usbDeviceConnect();
-    sei(); // Enable interrupts after re-enumeration
-    CHB_LCDstringLine(usbinit,0);
+    sei(); 							// Enable interrupts after re-enumeration
+    CHB_LCDstringLine(usbinit,0);	// USB ready to go!
 
-	// Feed watchdog and poll USB forever
-    while(1) {
+    while(1) {					// Feed watchdog and poll USB forever
         wdt_reset();
         usbPoll();
     }
@@ -66,37 +75,54 @@ int main(void)
 
 // Automagically called upon receiving a USB control message
 USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
-    usbRequest_t *rq = (void *)data; // cast data to correct type
+	// Clean up the data we received
+    usbRequest_t *rq = (void *)data;
 	uchar outputID;
 	int cmdID;
 	outputID = (rq->bRequest);
 	cmdID = (rq->wValue.bytes[0]);
 
-	if (outputID==0)
+	// Lighting and fan related commands
+	if (outputID==0)				// 0: Turn backlights off
 		set_low(PORTD,7);
-	else if(outputID==1)
+	else if(outputID==1)			// 1: Turn backlights on
 		set_high(PORTD,7);
-	else if(outputID==2){
+	else if(outputID==2){			// 2: Clear both displays
 		CHA_LCDclr();
 		CHB_LCDclr();
-	} else if(outputID==10){
-		// Expecting a string! Clear buffer and set index.
+	}
+	else if(outputID==3){			// 3: Adjust fan 1 speed
+		if(cmdID==0)
+			TCCR0A = TCCR0A&0xDF;	// Turn off completely if we got a zero
+		else{
+			OCR0B = cmdID;			// Take PWM value
+			TCCR0A = TCCR0A|0x20;	// Make sure this channel PWM is on
+		}
+	}
+	else if(outputID==5){			// 5: Adjust light speed
+		if(cmdID==0)
+			TCCR0A = TCCR0A&0x7F;	// Turn off completely if we got a zero
+		else{
+			OCR0A = cmdID;			// Take PWM value
+			TCCR0A = TCCR0A|0x80;	// Make sure this channel PWM is on
+		}
+	}
+
+	// LCD-related commands
+	else if(outputID==6)			// 6: Post buffer to LCD A, line 0
+		CHA_LCDstringLine(buff,0);
+	else if(outputID==7)			// 7: Post buffer to LCD A, line 1
+		CHA_LCDstringLine(buff,1);
+	else if(outputID==8)			// 8: Post buffer to LCD B, line 0
+		CHB_LCDstringLine(buff,0);
+	else if (outputID==9)			// 9: Post buffer to LCD B, line 1
+		CHB_LCDstringLine(buff,1);
+	else if(outputID==10)			// 10: String incoming. Reset buffer index.
 		addr = 0;
-	} else if(outputID==11){
-		// Got a char. Store it in the buffer.
+	else if(outputID==11){			// 11: Got a char. Store it in the buffer.
 		buff[addr] = cmdID;
 		addr++;
 	}
-
-	// For the rest, we finished receiving the string; post it to proper line.
-	else if(outputID==6)
-		CHA_LCDstringLine(buff,0);
-	else if(outputID==7)
-		CHA_LCDstringLine(buff,1);
-	else if(outputID==8)
-		CHB_LCDstringLine(buff,0);
-	else if (outputID==9)
-		CHB_LCDstringLine(buff,1);
 
 	return 0;
 }
